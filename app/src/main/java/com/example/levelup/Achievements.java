ff8c7d2fa4b1237a3bb478cbee51681da6a17b67
RachelTan07 <Rachel.Tan07@myhunter.cuny.edu>
2020-03-07 18:52:22 -0500

    navbar

--- app/src/main/java/com/example/levelup/Achievements.java
+++ app/src/main/java/com/example/levelup/Achievements.java
@@ -5,15 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class Training extends AppCompatActivity {
+public class Achievements extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getSupportActionBar().hide();
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_training);
+        setContentView(R.layout.activity_achievements);
     }
 }
